5f841d2bbbdf9ef3dcdcee064dbf931c0cfd3f1c
younghea <dydgo9507@gmail.com>
2018-08-21 18:14:48 +0900

    mobile부분 css 완성 및 grand page 수정해야함

---
+++ js/mobileNav.js
@@ -0,0 +1,20 @@
+// moblie일 때 nav부분
+let nav = true;
+
+$(function() {
+  $('.navIcon').click(function(){
+    if(nav){
+      $('.navIcon').children('img').attr('src', function ( index, attr ) {
+          return attr.replace('icon_nav.svg','icon_nav_on.svg');
+      });
+      $('.headerNav').addClass('on');
+      nav = false;
+    } else {
+      $('.navIcon').children('img').attr('src',function ( index, attr ) {
+          return attr.replace('icon_nav_on.svg','icon_nav.svg');
+      });
+      $('.headerNav').removeClass('on');
+      nav = true;
+    }
+  });
+});
